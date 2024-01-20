e02071b60c4d414b70d85a76f4bc3629af1f5cd3
Samuel Novaes <samuel.novaes.96@gmail.com>
2024-01-20 13:52:37 -0300

    fix json fields

--- src/entities/todo/model.go
+++ src/entities/todo/model.go
@@ -3,7 +3,7 @@ package todo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Todo struct {
-	Id        primitive.ObjectID `form:"id" bson:"_id"`
-	Title     string             `form:"title" bson:"title"`
-	Completed bool               `form:"completed" bson:"completed"`
+	Id        primitive.ObjectID `json:"id" bson:"_id"`
+	Title     string             `json:"title" bson:"title"`
+	Completed bool               `json:"completed" bson:"completed"`
 }
